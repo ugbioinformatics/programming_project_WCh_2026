a71d13d534b0894bce2b50da303f882cb08e7a57
czarek <cezary.czaplewski@ug.edu.pl>
2026-03-24 14:26:04 +0100

    test

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+
+def user_directory_path(instance, filename):
+     # file will be uploaded to MEDIA_ROOT/<id>/plik.pdb
+      return '{0}/{1}'.format(instance.id, 'plik.txt')
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+#    author = models.ForeignKey(
+#        "auth.User",
+#        on_delete=models.CASCADE,
+#    )
+    author = models.CharField(max_length=20,default='')
+    body = models.TextField()
+    suma = models.IntegerField(default=0)
+    plik1 = models.FileField(default='',upload_to=user_directory_path)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+        
