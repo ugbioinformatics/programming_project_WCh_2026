7d144269f2ccd524df6df21de131accb4f900d9f
polokolo122 <pgawski@gmail.com>
2026-03-31 13:51:16 +0200

    Add XTBInputForm for handling input types

--- blog/forms.py
+++ blog/forms.py
@@ -28,3 +28,9 @@ class Suma(forms.Form):
             except:
                 self.add_error('body','zła lista')
 
+class XTBInputForm(forms.Form):
+    INPUT_CHOICES = [('smiles', 'SMILES'), ('xyz', 'Plik XYZ')]
+    input_type = forms.ChoiceField(choices=INPUT_CHOICES, widget=forms.RadioSelect)
+    smiles = forms.CharField(required=False, label='SMILES',
+                             widget=forms.TextInput(attrs={'placeholder': 'np. CC(=O)O'}))
+    xyz_file = forms.FileField(required=False, label='Plik .xyz')
