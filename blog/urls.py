d5b8b15b39c97eb990647007ab3c8cc5290a9d28
czarek <cezary.czaplewski@ug.edu.pl>
2026-03-31 16:45:57 +0200

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BlogListView,BlogDetailView,BlogCreateView,suma,BlogUpdateView,BlogDeleteView
+
 from . import views
 
 from .views import BlogListView,BlogDetailView,BlogCreateView,suma,BlogDeleteView
@@ -10,7 +10,7 @@ urlpatterns = [
    path("", BlogListView.as_view(), name="home"),
    path("post/new/", BlogCreateView.as_view(), name="post_new"), # new  
    path("post/suma/", suma, name="suma"), 
-   path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+
    path("post/<int:pk>/delete/", BlogDeleteView.as_view(),name="post_delete"),
    path('xtb/', views.xtb_calc_view, name='xtb_calc'),
  
