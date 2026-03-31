56633706665f479f2da0dbc0db125e9789491875
polokolo122 <pgawski@gmail.com>
2026-03-31 13:32:30 +0200

    Delete xtb_app directory

--- xtb_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class XtbAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'xtb_app'
-    verbose_name = 'xTB Obliczenia'
