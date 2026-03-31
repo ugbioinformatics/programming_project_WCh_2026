f8919f46873aa8754c02e20d6db0b100ce807437
polokolo122 <pgawski@gmail.com>
2026-03-31 14:03:00 +0200

    all

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BlogListView,BlogDetailView,BlogCreateView,suma,BlogUpdateView,BlogDeleteView
+from . import views
 urlpatterns = [
    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
    path("", BlogListView.as_view(), name="home"),
