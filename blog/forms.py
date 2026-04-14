a740b21d8b981257938f10316db74bd57cd0cd85
olekmytys <olek.mytys@gmail.com>
2026-04-14 16:34:19 +0200

    rdkit

--- blog/forms.py
+++ blog/forms.py
@@ -2,17 +2,17 @@ from django import forms
 from .models import Post
 
 class Suma(forms.Form):
-    pole_smiles = forms.CharField(label='SMILES', required = False,widget=forms.TextInput(attrs={'size':40, 'maxlength':400}))
+    smiles = forms.CharField(label='SMILES', required = False,widget=forms.TextInput(attrs={'size':40, 'maxlength':400}))
     plik = forms.FileField(label='plik z danymi',required=False)
     
     def clean(self):
         cleaned_data = super(Suma, self).clean()
         plik=cleaned_data.get("plik")
-        pole_smiles=cleaned_data.get("pole_smiles") 
-        if pole_smiles and plik:
-        	self.add_error('pole_smiles', "Zdecyduj sie")
-        if not pole_smiles and not plik:
-        	self.add_error('pole_smiles', "Wpisz cos") 
+        smiles=cleaned_data.get("smiles")
+        if smiles and plik:
+        	self.add_error('smiles', "Zdecyduj sie")
+        if not smiles and not plik:
+        	self.add_error('smiles', "Wpisz cos")
 
 class XTBInputForm(forms.Form):
     INPUT_CHOICES = [('smiles', 'SMILES'), ('xyz', 'Plik XYZ')]
