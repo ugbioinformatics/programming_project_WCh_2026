dd667a080cf217c3dda358fa6a280debf642b099
olekmytys <olek.mytys@gmail.com>
2026-04-28 13:33:13 +0200

    konwersja do mol2

---
+++ a.py
@@ -0,0 +1,294 @@
+
+def count_atom_xtb(container):
+    nvib = 0
+    f1=[]
+    f2=[]
+    flag_1 = " Atom AN      X"
+    flag_2 = "                  4"
+    for i in range(len(container)):
+        line = container[i]
+        if "Frequencies --" in line:
+           if len(line)>60:
+              nvib = nvib + 3
+           elif len(line)>40:
+              nvib = nvib + 2
+           else:
+              nvib = nvib + 1
+        if flag_1 in line:
+           f1.append(i)
+        if flag_2 in line:
+           f2.append(i)
+    if len(f2) != 0:
+       natom = f2[0]-f1[0]-1
+
+    j=f1[0]
+    # nvib = 1,natom = 2
+    line3 = container[j+3]
+    if line3[0:4] != '   3':
+       natom = 2
+    # natom = 3, nvib =3
+    if line3[0:4] == '   3':
+       if (j+4) >= (len(container)-1):
+          natom = 3
+
+    #print(nvib,natom)
+    return nvib,natom
+
+def extract_freq_xtb(container): # for xtb
+    frq_list = []
+    sym_line_num = []
+    for i in range(len(container)):
+        line = container[i]
+        if "Frequencies" in line:
+            sym_line_num.append(i-1)
+            b = line.split()[2:]
+            for j in range(len(b)):
+                frq_list.append( round(float(b[j]),1) )
+
+    sym_list = []
+    for i in range(len(sym_line_num)):
+        j = sym_line_num[i]
+        b = container[j].split()
+        for k in range(len(b)):
+            sym_list.append(b[k])
+
+
+    return frq_list,sym_list
+
+
+def extract_mode_xtb(container,natom,nvib):
+    mode_raw = []
+    flag_1 =  "Atom AN      X"
+    mode_grid=[ ]
+
+
+    ready=0
+    for i in range(len(container)):
+        line = container[i]
+        if flag_1 in line:
+           ready = 1
+           continue
+        if ready > 0 and ready<=natom:
+           #print(line)
+           b = line.split()[2:]
+           b = [float(j) for j in b]
+
+           mode_grid.append(b)
+
+           ready = ready + 1
+           continue
+        if ready>natom:
+           ready = 0
+
+    #
+    #print("mode_grid")
+    #print(mode_grid)
+    for i in range(nvib):
+
+
+        j = i + 1
+        row = i / 3
+        row = int(row)
+        col = j % 3
+        if col == 0:
+           col = 3
+        col = col - 1
+
+        this_vib = []
+        #row:
+        # natom*row - start
+
+        #col:
+        # 3*col - start
+        for r in range(natom):
+           per_line = []
+           for c in range(3):
+              per_line.append( mode_grid[ natom*row+r][ 3*col+c ] )
+           this_vib.append(per_line)
+        #print("this_vib")
+        #print(this_vib)
+
+        mode_raw.append(this_vib)
+
+    #
+    return mode_raw
+
+
+def parse_xtb(path):
+
+    flag_1 = "and normal coordinates"
+    container = []
+    switch_1 = 0
+    with open(path) as f:
+         for line in f:
+             if flag_1 in line:
+                switch_1 = 1
+                continue
+             if switch_1 == 1:
+                if len(line) > 3:
+                   container.append(line)
+
+    nvib,natom = count_atom_xtb(container)
+    freqs,syms = extract_freq_xtb(container)
+
+    modes = []
+    modes=extract_mode_xtb(container,natom,nvib)
+
+    return freqs,modes,syms
+
+def load_xtb_xyz(path):
+#def load_xtb_xyz(path,obj_name): #chyba mozna bez problemu usunąć bo nie jest nigdy uzywane !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        flag_1 = "Number     Number      Type              X           Y           Z"
+        flag_2 = "------------------"
+
+        elem = []
+        elem_n = []
+        coor = []
+
+        lab = 0
+        have_full_elem = 0
+        info_collects = []
+        with open(path) as f:
+             for line in f:
+                 if flag_1 in line:
+                     lab = 1
+                     continue
+                 if lab == 1:
+                    lab = 2
+                    this_geom = []
+                    continue
+                 if lab == 2:
+                    #this_geom = []
+                    if flag_2 in line:
+                       lab = 0
+                       have_full_elem = 1
+                       info_collects.append( this_geom )
+                       continue
+                    #
+                    if have_full_elem == 0:
+                       e = int(line.split()[1])
+                       elem.append( get_symbol(e) )
+                    x = str( line.split()[3] )
+                    y = str( line.split()[4] )
+                    z = str( line.split()[5] )
+                    this_geom.append([x,y,z])
+
+                    continue
+
+        #
+        #print(elem)
+        coor = info_collects[-1]
+        #print(coor)
+
+        natom = len(elem)
+
+        f1 = open("xtb_geom_pymol_long_filename_lol.xyz","w")
+        f1.write(str(natom)+"\n"+"title\n")
+        for i in range(natom):
+            f1.write(elem[i]+" ")
+            f1.write(coor[i][0] + " ")
+            f1.write(coor[i][1] + " ")
+            f1.write(coor[i][2] + "\n")
+        f1.close()
+
+#        cmd.load("xtb_geom_pymol_long_filename_lol.xyz",obj_name)
+#        os.remove("xtb_geom_pymol_long_filename_lol.xyz")
+
+
+
+        return(coor,elem)
+
+
+def get_symbol(n):
+        i = n - 1;
+
+        s = ["H","He",\
+             "Li","Be","B","C","N","O","F","Ne",\
+             "Na","Mg","Al","Si","P","S","Cl","Ar",\
+             "K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",\
+             "Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe",\
+             "Cs","Ba",     "La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",\
+                      "Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",\
+             "Fr","Ra",     "Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",\
+             "Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"\
+             ] #
+        #
+        return s[i]
+
+def play_vib(xyz,mode,elem, out_file="vib_xyz.xyz"): # load up the frames for a vibration of primitive cell
+        import numpy as np
+        import math
+
+        #declare global variables
+        global global_amplitude
+
+
+#        cmd.delete("this_vib") # delete existing before creating a new one
+#        form.check_disp.setEnabled(True) # enable the check box for displacement vectors
+                                         # therefore, it indicates that we can only show displacement vectors after
+                                         # mode information is prepared
+
+        #obj_list = cmd.get_object_list('all')
+
+
+        cf = math.pi/180 # conversion factor
+        #sf = math.sin(90*cf*x)
+
+#        model_o = cmd.get_model('geom', 1) # input starting geometry
+
+        natom = len(elem)
+
+        #xyz_arr = np.array(np.float_(xyz)) <-nie dziala bo jest stary ale jak nie dzial to moze to cofnąć????????????????????????????????????????????????
+        xyz_arr = np.array(xyz, dtype=float)
+
+        mod_arr = np.array(mode) # is it correct -> corrected
+
+        mod_arr = mod_arr/np.linalg.norm(mod_arr) #normalize to 1
+
+        nslice = 6 #20 # number of frames for vibration animation
+        namplitude = global_amplitude #0.75
+        xyz_slices = []
+
+        for i in range(nslice+1):  #0-20
+            current_xyz = xyz_arr + math.sin(90*cf*float(i)/float(nslice)) *mod_arr*namplitude
+            xyz_slices.append(current_xyz)
+
+        for i in range(nslice): # 0-19
+            j = nslice-i-1 # 19-0
+            current_xyz = xyz_arr + math.sin(90*cf*float(j)/float(nslice)) *mod_arr*namplitude
+            xyz_slices.append(current_xyz)
+        for i in range(1,nslice+1): # 1-20
+            j = -1*i # -1 ~ -20
+            current_xyz = xyz_arr + math.sin(90*cf*float(j)/float(nslice)) *mod_arr*namplitude
+            xyz_slices.append(current_xyz)
+        for i in range(1,nslice):#1-19
+            k = nslice -i
+            j = -1*k # -19 ~ -1
+            current_xyz = xyz_arr + math.sin(90*cf*float(j)/float(nslice)) *mod_arr*namplitude
+            xyz_slices.append(current_xyz)
+
+
+
+
+        # generate xyz file
+        n_states = len(xyz_slices)
+        f1=open(out_file,"w")
+        for i in range(n_states):
+            f1.write(str(natom)+"\ntitle\n")
+            for j in range(natom):
+                f1.write(str(elem[j])+" ")
+                f1.write(str(xyz_slices[i][j][0])+" ")
+                f1.write(str(xyz_slices[i][j][1])+" ")
+                f1.write(str(xyz_slices[i][j][2])+"\n")
+        f1.close()
+
+import subprocess
+freqs,modes,syms=parse_xtb('g98.out')
+#xyz,elem=load_xtb_xyz('g98.out',"geom") stare mozna usunąć !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+xyz,elem=load_xtb_xyz('g98.out')
+global_amplitude=0.4
+
+
+for i, mode in enumerate(modes):
+    play_vib(xyz, mode, elem, f"vib_{i}.xyz")
+    subprocess.run(["/usr/bin/obabel", "-ixyz", f"vib_{i}.xyz", "-o", "mol2", "-O", f"vib_{i}.mol2"])
