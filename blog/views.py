1c80bf2555c19b28327f695e5910086f75e9ddf1
olekmytys <olek.mytys@gmail.com>
2026-04-29 14:00:23 +0200

    delete xyz files

--- blog/views.py
+++ blog/views.py
@@ -159,6 +159,183 @@ def run_hess(tmpdir):
                         except ValueError:
                             pass
 
+
+    def get_symbol(n):
+        s = ["H","He",
+             "Li","Be","B","C","N","O","F","Ne",
+             "Na","Mg","Al","Si","P","S","Cl","Ar",
+             "K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+             "Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe",
+             "Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+             "Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
+             "Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",
+             "Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"]
+        return s[n - 1]
+
+    def count_atom_xtb(container):
+        nvib = 0
+        f1 = []
+        f2 = []
+        flag_1 = " Atom AN      X"
+        flag_2 = "                  4"
+        for i, line in enumerate(container):
+            if "Frequencies --" in line:
+                if len(line) > 60:
+                    nvib += 3
+                elif len(line) > 40:
+                    nvib += 2
+                else:
+                    nvib += 1
+            if flag_1 in line:
+                f1.append(i)
+            if flag_2 in line:
+                f2.append(i)
+        natom = f2[0] - f1[0] - 1 if f2 else 0
+        j = f1[0]
+        line3 = container[j + 3]
+        if line3[0:4] != '   3':
+            natom = 2
+        elif (j + 4) >= (len(container) - 1):
+            natom = 3
+        return nvib, natom
+
+    def extract_freq_xtb(container):
+        frq_list = []
+        sym_line_num = []
+        for i, line in enumerate(container):
+            if "Frequencies" in line:
+                sym_line_num.append(i - 1)
+                for val in line.split()[2:]:
+                    frq_list.append(round(float(val), 1))
+        sym_list = []
+        for i in sym_line_num:
+            sym_list.extend(container[i].split())
+        return frq_list, sym_list
+
+    def extract_mode_xtb(container, natom, nvib):
+        flag_1 = "Atom AN      X"
+        mode_grid = []
+        ready = 0
+        for line in container:
+            if flag_1 in line:
+                ready = 1
+                continue
+            if 0 < ready <= natom:
+                mode_grid.append([float(v) for v in line.split()[2:]])
+                ready += 1
+                continue
+            if ready > natom:
+                ready = 0
+        mode_raw = []
+        for i in range(nvib):
+            row = i // 3
+            col = (i + 1) % 3
+            if col == 0:
+                col = 3
+            col -= 1
+            this_vib = [
+                [mode_grid[natom * row + r][3 * col + c] for c in range(3)]
+                for r in range(natom)
+            ]
+            mode_raw.append(this_vib)
+        return mode_raw
+
+    def parse_xtb(path):
+        flag_1 = "and normal coordinates"
+        container = []
+        reading = False
+        with open(path) as f:
+            for line in f:
+                if flag_1 in line:
+                    reading = True
+                    continue
+                if reading and len(line) > 3:
+                    container.append(line)
+        nvib, natom = count_atom_xtb(container)
+        freqs, syms = extract_freq_xtb(container)
+        modes = extract_mode_xtb(container, natom, nvib)
+        return freqs, modes, syms
+
+    def load_xtb_xyz(path):
+        flag_1 = "Number     Number      Type              X           Y           Z"
+        flag_2 = "------------------"
+        elem = []
+        lab = 0
+        have_full_elem = False
+        info_collects = []
+        this_geom = []
+        with open(path) as f:
+            for line in f:
+                if flag_1 in line:
+                    lab = 1
+                    continue
+                if lab == 1:
+                    lab = 2
+                    this_geom = []
+                    continue
+                if lab == 2:
+                    if flag_2 in line:
+                        lab = 0
+                        have_full_elem = True
+                        info_collects.append(this_geom)
+                        continue
+                    if not have_full_elem:
+                        elem.append(get_symbol(int(line.split()[1])))
+                    this_geom.append([line.split()[3], line.split()[4], line.split()[5]])
+        coor = info_collects[-1]
+        natom = len(elem)
+        geom_path = os.path.join(tmpdir, 'xtb_geom.xyz')
+        with open(geom_path, 'w') as f1:
+            f1.write(f"{natom}\ntitle\n")
+            for i in range(natom):
+                f1.write(f"{elem[i]} {coor[i][0]} {coor[i][1]} {coor[i][2]}\n")
+        return coor, elem
+
+    def play_vib(xyz, mode, elem, amplitude=0.4):
+        import numpy as np
+        import math
+        cf = math.pi / 180
+        natom = len(elem)
+        xyz_arr = np.array(xyz, dtype=float)
+        mod_arr = np.array(mode)
+        mod_arr = mod_arr / np.linalg.norm(mod_arr)
+        nslice = 6
+        xyz_slices = []
+        for i in range(nslice + 1):
+            xyz_slices.append(xyz_arr + math.sin(90 * cf * i / nslice) * mod_arr * amplitude)
+        for i in range(nslice):
+            xyz_slices.append(xyz_arr + math.sin(90 * cf * (nslice - i - 1) / nslice) * mod_arr * amplitude)
+        for i in range(1, nslice + 1):
+            xyz_slices.append(xyz_arr + math.sin(90 * cf * (-i) / nslice) * mod_arr * amplitude)
+        for i in range(1, nslice):
+            xyz_slices.append(xyz_arr + math.sin(90 * cf * (-(nslice - i)) / nslice) * mod_arr * amplitude)
+
+        frames = []
+        for frame in xyz_slices:
+            lines = [f"{natom}\ntitle\n"]
+            for j in range(natom):
+                lines.append(f"{elem[j]} {frame[j][0]} {frame[j][1]} {frame[j][2]}\n")
+            frames.append("".join(lines))
+        return frames
+
+    freqs, modes, syms = parse_xtb(g98_path)
+    xyz, elem = load_xtb_xyz(g98_path)
+
+    vib_dir = os.path.join(tmpdir, "vibrations")
+    os.makedirs(vib_dir)
+    mol2_files = []
+    for i, mode in enumerate(modes):
+        vib_xyz  = f"{vib_dir}/vib_{i}.xyz"
+        vib_mol2 = f"{vib_dir}/vib_{i}.mol2"
+        frames = play_vib(xyz, mode, elem)
+        with open(vib_xyz, 'w') as fh:
+            fh.write("".join(frames))
+        subprocess.run(["/usr/bin/obabel", "-ixyz", vib_xyz, "-omol2", "-O", vib_mol2],capture_output=True, cwd=vib_dir)
+        os.remove(vib_xyz)
+        if os.path.exists(vib_mol2):
+            mol2_files.append(vib_mol2)
+
+
     return {
         'frequencies': frequencies,
         'hess_log': hess_log,
