05f8287f6c8dab621ed936abd7f5223a62a566e5
tymek402 <tymek.warchol@gmail.com>
2026-05-26 15:16:52 +0200

    nie wiem

--- blog/views.py
+++ blog/views.py
@@ -423,7 +423,6 @@ def run_hess(tmpdir):
 
             with open(f'{vib_dir}/link_list.html', 'w', encoding='utf-8') as t:
 
-                t.write("<pre>")
 
                 t.write('''
 <a href="javascript:history.back()"
@@ -469,6 +468,15 @@ transition:all 0.2s ease;
 </svg>
 </button>
 ''')
+                t.write('''
+<div class="grid" style="
+    display:grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap:10px;
+    max-width:1300px;
+    margin:auto;
+">
+''')
                 
                 for i, line in enumerate(content):
 
@@ -487,7 +495,7 @@ transition:all 0.2s ease;
     width:100%;
     box-sizing:border-box;
     text-align:center;
-    background:#e5e7eb;;
+    background:#e5e7eb;
     color:black;
     text-decoration:none;
     padding:12px 16px;
@@ -648,7 +656,8 @@ document.addEventListener("DOMContentLoaded", function () {{
                         if "placeholder" in text:
                             text = text.replace("placeholder", f"vib_{i}.mol2")
                         d.write(text)
-                t.write("</pre>")
+                t.write("</div>")
+
 
     freqs, modes, syms = parse_xtb(g98_path)
     xyz, elem = load_xtb_xyz(g98_path)
